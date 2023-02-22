ce656bcf48d2065fee7e16e808127dfaf09ba2fb
linhhl2807 <118676532+linhhl2807@users.noreply.github.com>
2023-02-22 12:43:47 +0700

    Add files via upload

---
+++ BTTH2.sql
@@ -0,0 +1,20 @@
+﻿select *
+from NHANVIEN
+where PHG = 4	
+GO
+select *
+from NHANVIEN
+where LUONG > 30000
+GO
+select *
+from NHANVIEN
+where ( LUONG > 25000 AND PHG = 4 ) OR ( LUONG > 30000 AND PHG = 5 )
+GO
+select HONV, TENLOT, TENNV, DCHI
+from NHANVIEN
+where DCHI LIKE '%TP HCM' OR DCHI LIKE '%Tp Hồ Chí Minh'
+GO
+select HONV, TENLOT, TENNV
+from NHANVIEN
+where HONV LIKE 'N%'
+GO
